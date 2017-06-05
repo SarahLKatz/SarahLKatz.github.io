cfd5ca5496e71fb7f08503fcebfeca8d123c9fa2
Sarah L. Katz <soshasarah@gmail.com>
2017-06-05 17:44:12 -0400

    Magic 8 Ball - Mobile Site
    Made some changes to navigation (and corresponding styles and js) for
    the responsiveness of the site

--- other/magic_eight/js/main.js
+++ other/magic_eight/js/main.js
@@ -1,4 +1,5 @@
 $(function() {
+
   var answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", 
   "As I see it yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", 
   "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", 
@@ -86,10 +87,11 @@ $(function() {
     $("#displayed").attr("src", newPath)
   };
 
-  $("nav button").click(function(){
+  $("a").click(function(){
     var color = $(this).attr("id")
     $("body").removeClass();
     $("body").addClass(color);
+    $('.navbar-toggle').click()
   });
 
 });
