720e5a68ff81dedd2dd1d80dff666018990f886e
Sarah Katz <soshasarah@gmail.com>
2019-04-01 17:17:56 -0400

    2010-present
    Moves events from 2010 to end of timeline to a JS file

--- timeline/2010.js
+++ timeline/2010.js
@@ -1,5 +1,238 @@
-var events = [
-  {
+ var events = [{
+    park: "ep",
+    date: "May 9, 2010",
+    title: "Honey, I Shrunk the Audience Closes",
+    description:
+      "Located in Epcot's Future World, this 4-D film is a spin off of the movie <em>Honey, I Shrunk the Kids</em>. The attraction opened on November 21, 1994, and was replaced by a new version of the attraction it originally replaced, Captain EO."
+  } , {
+    park: "mk",
+    date: "June 1, 2010",
+    title: "Cinderella's Golden Carrousel Becomes Prince Charming Regal Carrousel",
+    description:
+      "The carousel, located in Magic Kingdom's Fantasyland, is renamed to better fit the story behind the attraction. According to the official backstory, the carousel was inspired by a device built by Prince Charming to assist him in training for the ring-spearing event at jousting matches. A second carousel was built closer to the castle to allow villagers to try their hand at this device. With this new backstory, park visitors were invited to test their horsemanship skills and to enjoy their own happy ending while riding the carousel."
+  } , {
+    park: "ep",
+    date: "July 2, 2010",
+    title: "<em>Captain EO</em> Tribute Opens at Epcot",
+    description:
+      "This 4-D film, a new presentation of the film <em>Captain EO</em> (which originally ran at Epcot from 1986-1994), was brought back to Disney Parks around the world after the death of its star, Michael Jackson. The film was directed by Francis Ford Coppola and produced by George Lucas. <em>Captain EO</em> Tribute closed on December 6, 2015, to make room for a new attraction featuring Disney and Pixar short films."
+  } , {
+    park: "rD",
+    date: "October 2, 2010",
+    title: "Inaugural runDisney Wine & Dine Half-Marathon Weekend Begins",
+    description:
+      "This race weekend, themed around Epcot's International Food and Wine Festival, featured Mickey's Halloween Family Fun Run 5K and the Wine and Dine Half Marathon, as well as two-day expo and a Food & Wine Festival Finish Line Party. With a 10 PM start, this was runDisney's first nighttime half marathon, and offered a relay option for runners who did not wish to complete the first 13.1 miles."
+  } , {
+    park: "dhs",
+    date: "May 20, 2011",
+    title: "Star Tours: The Adventure Continues Opens at Hollywood Studios",
+    description:
+      "This 3D motion simulator, an updated version of the attraction Star Tours, takes guests inside the world of Star Wars. Guests travel through locations seen in Star Wars Episodes I - VII as droids C-3PO and R2-D2 attempt to safely return a rebel spy to the Rebel Alliance."
+  } , {
+    park: "mk",
+    date: "August 15, 2011",
+    title: "The Enchanted Tiki Room Reopens at Magic Kingdom",
+    description:
+      "This show, which originally ran from October 1971 to September 1997, is a slightly edited version of the classic Disneyland attraction. The show features over 150 talking, singing and dancing birds, flowers, a magic fountain, tiki drummers and tiki totem poles, all singing the attraction's signature tunes, including 'The Tiki Tiki Tiki Room' by the Sherman Brothers."
+  } , {
+    park: "dhs",
+    date: "May 18, 2012",
+    title: "Sounds Dangerous! Closes at Hollywood Studios",
+    description:
+      "This audio show, located in Echo Lake section of the park, opened on April 22, 1999. The show starred comedian Drew Carey and relied almost entirely on audio effects with few visuals."
+  } , {
+    park: "resort",
+    date: "May 31, 2012",
+    title: "Disney's Art of Animation Resort Opens",
+    description:
+      "The value resort, located near ESPN's Wide World of Sports Complex and built on the land that was originally intended for the 'Classic Years' section of Pop Century resort, features both family suites and motel-style standard rooms, divided into four themed areas. The <em>Finding Nemo</em>, <em>Cars</em>, and <em>The Lion King</em> areas all house family suites, while <em>The Little Mermaid</em> section contains standard hotel rooms. The resort has a food court, souvenir shop, arcade room, pool bar, and three pools, and is connected to the Pop Century Resort by the Generation Gap Bridge."
+  } , {
+    park: "mk",
+    date: "October 4, 2012",
+    title: "Pete's Silly Sideshow and Big Top Souvenirs Open in Magic Kingdom's Storybook Circus",
+    description:
+      "Pete's Silly Sideshow, a character meet and greet area, features The Astounding Donaldo (Donald Duck), The Great Goofini (Goofy), Madame Daisy Fortuna (Daisy Duck), and Minnie Magnifique (Minnie Mouse). Big Top Souvenirs is a large circus tent that sells various souvenirs, including various Disney-themed caramel apples."
+  } , {
+    park: "mk",
+    date: "November 9, 2012",
+    title: "Be Our Guest Restaurant Opens at Magic Kingdom",
+    description:
+      "This restaurant, located in Beast's Castle and themed around the animated film <em>Beauty and the Beast</em>, contains three rooms in which guests can eat: the West Wing, the Ballroom, and the Rose Gallery. The restaurant is known for its signature 'grey stuff' and was the first restaurant in Magic Kingdom to serve alcohol (at dinner only)."
+  } , {
+    park: "mk",
+    date: "December 6, 2012",
+    title: "New Fantasyland Opens at Magic Kingdom",
+    description:
+      "A special ceremony celebrated the official opening of Magic Kingdom's New Fantasyland, an expansion and renovation originally announced in 2009. New attractions that opened on this day included Ariel's Grotto (a meet and greet area), Under the Sea: Journey of the Little Mermaid (an dark ride that tells the story of <em>The Little Mermaid</em>), and Enchanted Tales with Belle (an interactive show where guests help tell the story of Belle and the Beast)."
+  } , {
+    park: "dhs",
+    date: "December 6, 2012",
+    title: "The Legend of Captain Jack Sparrow Opens at Hollywood Studios",
+    description:
+      "This walk-through attraction, located in the area that formerly housed Journey into Narnia: Prince Caspian, follows the story of Captain Jack Sparrow from the <em>Pirates of the Caribbean</em> film series. The attraction closed on November 6, 2014, to make room for upcoming expansions at the park."
+  } , {
+    park: "mk",
+    date: "March 8, 2013",
+    title: "Tangled Rest Area Opens at Magic Kingdom",
+    description:
+      "This rest area, located in Fantasyland, allows guests to rest in a highly-themed area featuring Rapunzel's Tower, lanterns, wanted posters for ruffians, and many other elements from the film <em>Tangled</em>. In addition to restrooms, the area contains charging areas, benches, and a scavenger hunt to find hidden Pascals."
+  } , {
+    park: "mk",
+    date: "September 17, 2013",
+    title: "Princess Fairytale Hall Opens",
+    description:
+      "This meet and greet area, located in Magic Kingdom's Fantasyland, allows guests to meet various Disney Princesses. Princesses are subject to change, but in the past characters that meet in this area have included Cinderella, Elena, Rapunzel, Tiana, Anna, Elsa, Snow White, and Aurora."
+  } , {
+    park: "resort",
+    date: "October 23, 2013",
+    title: "The Villas at Disney's Grand Floridian Resort & Spa Opens",
+    description:
+      "TThis Disney Vacation Club property, located in the Magic Kingdom resort area, features its own checkin and concierge areas, but also shares many amenities with the Grand Floridian Resort & Spa. The villas and studios contain subtle theming to Dumbo the Flying Elephant and Mary Poppins."
+  } , {
+    park: "rd",
+    date: "January 9, 2014",
+    title: "Inaugural runDisney Dopey Challenge Begins",
+    description:
+      "The Dopey Challenge, part of Walt Disney World Marathon Weekend, includes four races over four days - the Family Fun Run 5K, the Walt Disney World 10K (new for 2014), the Walt Disney World Half Marathon, and the Walt Disney World Marathon. After running 48.6 miles, participants will have received a total of 6 medals - 4 individual race medals, the Goofy Race-and-a-Half Challenge medal (for completing the Half Marathon and Marathon), and the Dopey Challenge medal. Over 5,000 people complete the Dopey Challenge in its inaugural year."
+  } , {
+    park: "rd",
+    date: "February 22, 2014",
+    title: "Inagural Enchanted 10K Race",
+    description:
+      "The 2014 Princess Half Marathon Weekend features a new race, the Enchanted 10K, which runs through Epcot and the Epcot resort area. In addition to the new race, a challenge is added to the weekend. The Glass Slipper challenge awards a medal to registered participants who complete both the Enchanted 10K and Princess Half Marathon. Also introduced this year is the Pink Coast-to-Coast Medal, awarded to runners who complete the Tinker Bell Half Marathon at Disneyland and the Princess Half Marathon at Walt Disney World (this medal would be discontinued when Disneyland races went on hiatus in 2018)."
+  } , {
+    park: "mk",
+    date: "May 28, 2014",
+    title: "Seven Dwarfs Mine Train Opens at Magic Kingdom",
+    description:
+      "This steel roller coaster, the last attraction to open as part of Magic Kingdom's New Fantasyland expansion, is themed around the classic animated film <em>Snow White and the Seven Dwarfs</em>. The attraction, which contains both outdoor and indoor elements, operates with a new ride system with 'tilting' vehicles, and features a mixture of new audio-animatronics and figures recycled from a now-closed attraction, Snow White's Scary Adventures."
+  } , {
+    park: "dak",
+    date: "June 1, 2014",
+    title: "Festival of the Lion King at Animal Kingdom Reopens",
+    description:
+      "This show, which closed when its theater was demolished for an Avatar-themed expansion, is now hosted in the Harambe Theater in the Africa section of the park. Inspired by the film <em>The Lion King</em>, the show uses songs, dance, puppetry and visual effects to portray a tribal celebration in an African savanna setting filled with lions, elephants, giraffes, birds, zebras and gazelles."
+  } , {
+    park: "dhs",
+    date: "July 5, 2014",
+    title: "For the First Time in Forever: A Frozen Sing-Along Celebration Debuts",
+    description:
+      "This musical sing-along show, originally located in the Premiere Theater in the Streets of America section of Disney's Hollywood Studios, tells the story of the 2013 film <em>Frozen</em> with the help of new characters, the Royal Historians of Arendelle. In June 2015, the show moved to a new location, the Hyperion Theater in Echo Lake (formerly the home of the American Idol Experience)."
+  } , {
+    park: "dhs",
+    date: "September 27, 2014",
+    title: "Studio Backlot Tour at Hollywood Studios Closes",
+    description:
+      "The Studio Backlot Tour, which opened with the park on May 1, 1989, was a combination walking and tram tour of the backlot area of the park. The tour, which was changed and shortened over the years, closed with no announcement of what would replace it. It was eventually announced that the area would be used for expansions to the park."
+  } , {
+    park: "resort",
+    date: "April 1, 2015",
+    title: "Disney's Polynesian Villas & Bungalows Opens",
+    description:
+      "This newest Disney Vacation Club property, located in the Magic Kingdom reosrt area, features both studios and bungalows. The 'Bora Bora' Bungalows are located over the water, sleep up to 8 guests, and offer a view of Magic Kingdom and a private plunge pool."
+  } , {
+    park: "dhs",
+    date: "July 12, 2015",
+    title: "The Magic of Disney Animation at Hollywood Studios Closes",
+    description:
+      "This attraction, which opened with Disney-MGM Studios on May 1, 1989, and its building originally housed the Orlando, Florida division of Walt Disney Feature Animation. It featured exhibits exploring the history of Disney animation, The Animation Academy, and character meet and greets. No announcement about the future of the building was made when the attraction closed, but the building now houses the Star Wars Launch Bay."
+  } , {
+    park: "resort",
+    date: "September 29, 2015",
+    title: "Downtown Disney Becomes Disney Springs",
+    description:
+      "Walt Disney World's shopping, dining, and entertainment complex is given a new name and a new theme as part of a major expansion project. The complex is divided into four areas, Marketplace, The Landing, Town Center, and West Side, and features many new dining and shopping locations as part of its expansion and rebranding."
+  } ,{
+    park: "dhs",
+    date: "December 1, 2015",
+    title: "Jedi Training: Trials of the Temple Debuts at Hollywood Studios",
+    description:
+      "This show, an update of the Jedi Training Academy, allows children to train with a Jedi master and Apprentice. After training, the participants are given the opportunity to fight against <em>Star Wars</em> villains, including Darth Vader and The Seventh Sister (from <em>Star Wars Rebels</em>)."
+  } , {
+    park: "dhs",
+    date: "December 5, 2015",
+    title: "Star Wars Launch Bay Opens",
+    description:
+      "This attraction, located in the Animation Courtyard area of Hollywood Studios, features props from the <em>Star Wars</em> series of films. Additionally, character meet and greets are available, and Jawas wander around the pavillion looking to trade goods with guests."
+  } , {
+    park: "mk",
+    date: "December 16, 2015",
+    title: "Jungle Skipper Canteen Opens at Magic Kingdom",
+    description:
+      "Located in the Adventureland area of the park, this restaurant is themed around the nearby Jungle Cruise attraction. According to its backstory, the skippers of the Jungle Cruise have opened the kitchen at their headquarters to guests. The restaurant contains three rooms: the crew’s colonial-era Mess Hall; the Jungle Room, former family parlor of Dr. Albert Falls himself; and the S.E.A. Room—a once-secret meeting place for the Society of Explorers and Adventurers."
+  } , {
+    park: "ep",
+    date: "December 23, 2015",
+    title: "Disney & Pixar Short Film Festival Opens",
+    description:
+      "Located in Epcot's Future World, in the theater that formerly housed <em>Captain EO</em>, this attraction opened showing three Disney and Pixar short films: Get a Horse (Disney), For the Birds (Pixar), and La Luna (Pixar). 4D effects are used to enhance the films."
+  } , {
+    park: "dhs",
+    date: "April 2, 2016",
+    title: "Honey, I Shrunk the Kids: Movie Set Adventure Closes",
+    description:
+      "This kids' play area, locaed in the Streets of America section of Hollywood Studios, featured large movie props and structures inspired by the classic Disney film <em>Honey, I Shrunk the Kids</em>. It closed to make room for expansions at Hollywood Studios."
+  } , {
+    park: "rD",
+    date: "April 14, 2016",
+    title: "Inaugural Star Wars Half Marathon — The Dark Side Weekend Begins",
+    description:
+      "This race weekend, the second runDisney weekend with a <em>Star Wars</em> theme, includes three races: a 5K, a 10K, and the Star Wars Half Marathon – The Dark Side. Participants who registered for the Dark Side Challenge and completed both the 10K and Half Marathon races received a special challenge t-shirt and medal. Participants who completed both the Star Wars Half Marathon - The Light Side at Disneyland (in January 2016) and the Star Wars Half Marathon - The Dark Side at Walt Disney World received a special Kessel Run-themed medal (shaped like the Millenium Falcon)."
+  } , {
+    park: "dhs",
+    date: "June 17, 2016",
+    title: "Star Wars: A Galactic Spectacular Debuts at Hollywood Studios",
+    description:
+      "This nighttime spectacular, which replaced Symphony in the Stars: A Galactic Spectacular, features fireworks, projection mapping, fire, lasers, fog effects, and searchlights. Images from the <em>Star Wars</em> films are projected onto The Great Movie Ride's exterior Chinese Theater, and the fireworks and effects are set to music from the films."
+  } , {
+    park: "ep",
+    date: "June 21, 2016",
+    title: "Frozen Ever After Opens at Epcot",
+    description:
+      "Located in the Norway pavillion in World Showcase, this attraction features scenes inspired by Disney's 2013 animated film <em>Frozen</em> as well as the 2015 animated short <em>Frozen Fever</em>."
+  } , {
+    park: "mk",
+    date: "October 2, 2016",
+    title: "The Muppets Present...Great Moments in American History Premieres",
+    description:
+      "This live show, located in the Liberty Square area at the Magic Kingdom, is performed daily and features the Muppets presenting abridged accounts of notable milestones in American history, primarily from the Colonial era. The majority of the show unfolds from the second-story windows of the Heritage House, adjacent to the Hall of Presidents attraction."
+  } , {
+    park: "rD",
+    date: "November 5, 2016",
+    title: "Inaugural Wine and Dine 10K Race",
+    description:
+      "This inaugural race, represented by Chef Remy and added as part of a number of changes to the Wine and Dine Half Marathon Weekend, runs through Epcot and the Epcot resort area. In addition to the 10K, a challenge was added to the weekend, giving participants the opportunity to earn the Lumiere's Two-Course Challenge medal for completing both the 10K and Half Marathon (the half marathon, which had been held on Saturday night in previous years, was moved to Sunday morning)."
+  } , {
+    park: "dak",
+    date: "February 17, 2017",
+    title: "Rivers of Light Debuts at Animal Kingdom",
+    description:
+      "After several delays, this new nighttime show debuts at Animal Kingdom. Rivers of Light features water screens, fountains, lasers, music, boats, floats, and fire to show the colorful world of animals and nature."
+  } , {
+    park: "dhs",
+    date: "May 26, 2017",
+    title: "The Music of Pixar Live! A Symphony of Characters",
+    description:
+      "Presented nightly, this show featured scenes from popular Pixar films accompanied by orchestral music and highlighted some of the most popular Pixar tunes. The show's limited run ended on August 27, 2017."
+  } , {
+    park: "dak",
+    date: "May 27, 2017",
+    title: "Pandora – The World of Avatar Opens",
+    description:
+      "This new area at Animal Kingdom, inspired by the film <em>Avatar</em>, features two attractions, Avatar Flight of Passage and Na'vi River Journey, a themed quick service restaurant, and a food and beverage stand."
+  } , {
+    park: "dhs",
+    date: "August 13, 2017",
+    title: "The Great Movie Ride Closes",
+    description:
+      "This ride-through movie attraction, which opened with Disney-MGM Studios in 1989, used Audio-Animatronics, live actors, special effects, and static sets to recreate classic movie scenes. The attraction, which was housed in a building meant to resemble Graumann's Chinese Theater, will be replaced by a Mickey Mouse themed ride."
+  } , {
+    park: "dhs",
+    date: "September 27, 2017",
+    title: "BaseLine Tap House Opens at Hollywood Studios",
+    description:
+      "Located in the new Grand Avenue area, this pub specializes in beers and wine from California. Snacks served include Bavarian pretzel and California cheeses."
+  } , {
     park: "dhs",
     date: "June 30th, 2018",
     title: "Toy Story Land Opens",
