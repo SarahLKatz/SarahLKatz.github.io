41ee4db676dae08826f93bf929f1c459795321ad
Sarah Katz <soshasarah@gmail.com>
2019-04-08 13:48:23 -0400

    2000-2010 to JS Converts events from 2000 through 2009 to a JS file and removes from HTML file

---
+++ timeline/2000.js
@@ -0,0 +1,248 @@
+var aughtsEvents = [{
+    park: "resort",
+    date: "November 15, 2000",
+    title: "The Villas at Disney's Wilderness Lodge Opens",
+    description:
+      "The villas, a Disney Vacation Club property, will later be expanded and renamed Boulder Ridge Villas. The expansion will include waterside villas, new amenities, and new dining facilities."
+  } , {
+    park: "mk",
+    date: "April 1, 2001",
+    title: "SpectroMagic Debuts at Magic Kingdom",
+    description:
+      "The second incarnation of this popular nighttime parade runs through June 5, 2010, when it closes permanently."
+  } , {
+    park: "dhs",
+    date: "April 7, 2001",
+    title: "Who Wants To Be A Millionaire – Play It! Debuts at Disney-MGM Studios",
+    description:
+      "The attraction, a modified version of the <em>Who Wants to Be a Millionaire</em> television game show, closed on August 20, 2004."
+  } , {
+    park: "resort",
+    date: "April 16, 2001",
+    title: "Disney's Animal Kingdom Lodge Opens",
+    description:
+      "The resort, whose main building is renamed Jambo House when a Disney Vacation Club property opens as part of the resort, offers an opportunity to view wildlife in a savanna-type environment outside of public areas and certain guest rooms."
+  } , {
+    park: "mk",
+    date: "May 24, 2001",
+    title: "The Magic Carpets of Aladdin Opens",
+    description:
+      "The attraction, located in Magic Kingdom's Adventureland, is an aerial carousel ride that allows riders to control both the height of their carpet and the amount of tilt."
+  } , {
+    park: "mk",
+    date: "October 1, 2001",
+    title: "Share A Dream Come True Parade Debuts at Magic Kingdom",
+    description:
+      "This afternoon parade, which was renamed as the Disney Dreams Come True Parade in 2006 and again as Celebrate A Dream Come True in 2009, ended its final run on January 4th, 2014."
+  } , {
+    park: "mk",
+    date: "October 1, 2001",
+    title: "Cinderella's Surprise Celebration Debuts",
+    description:
+      "This show, which took place on a stage in front of Cinderella Castle at Magic Kingdom, went on hiatus on February 26, 2005, and never returned from that hiatus."
+  } , {
+    park: "dhs",
+    date: "October 1, 2001",
+    title: "Walt Disney: One Man’s Dream Opens at Disney-MGM Studios",
+    description:
+      "This interactive gallery attraction takes guests inside the world of Walt Disney and features artifacts from his life, as well as models of various Disney parks around the world. The attraction ends with a short film of Walt Disney's life."
+  } , {
+    park: "dhs",
+    date: "October 1, 2001",
+    title: "Playhouse Disney – Live on Stage Debuts at Disney-MGM Studios",
+    description:
+      "The show, which features beloved characters from Disney Junior shows, will later be renamed Disney Junior - Live on Stage!"
+  } , {
+    park: "dak",
+    date: "November 1, 2001",
+    title: "TriceraTop Spin Opens at Animal Kingdom",
+    description:
+      "This aerial carousel attraction, located in Dinoland U.S.A., allows riders to control both up/down and forward/backward motion."
+  } , {
+    park: "dak",
+    date: "March 31, 2002",
+    title: "Primeval Whirl Opens at Animal Kingdom",
+    description:
+      "This attraction, a steel wild mouse roller coaster located in the Chester and Hester's Dino-Rama portion of Dinoland U.S.A., is themed to time travel and to the meteor which is believed to have caused the extinction of the dinosaurs."
+  } , {
+    park: "ep",
+    date: "June 2, 2002",
+    title: "Journey into Imagination with Figment Opens at Epcot",
+    description:
+      "This attraction, the third incarnation of an imagination-themed attraction located in the Imagination! pavilion in Epcot's future world, features a small purple dragon named Figment, who is currently voiced by Muppeteer Dave Goelz."
+  } , {
+    park: "resort",
+    date: "July 1, 2002",
+    title: "Disney's Beach Club Villas, a Disney Vacation Club Resort, Opens",
+    description:
+      "The villas, located in the Epcot resort area, share their amenities, including Stormalong Bay (one of the most popular pools on Disney property), with the Beach Club and Yacht Club resorts."
+  } , {
+    park: "ep",
+    date: "May 22, 2003",
+    title: "Reflections of China Opens at Epcot",
+    description:
+      "This Circle-Vision 360° movie, located in the China pavilion in World Showcase, replaced the opening day attraction Wonders of China. The show is narrated by an actor playing Li Bai, an ancient Chinese poet, who takes the viewer on a tour of the Chinese countryside and historical structures and buildings."
+  } , {
+    park: "ep",
+    date: "August 15, 2003",
+    title: "Mission:Space Opens at Epcot",
+    description:
+      "This motion simulator attraction, which simulates what an astronaut might experience aboard a spacecraft on a mission to Mars, currently offers two experiences: Orange Team, which is more intense (and reflects the opening day attraction), and Green Team, which does not spin and is therefore a less intense experience."
+  } , {
+    park: "mk",
+    date: "October 8, 2003",
+    title: "Mickey's PhilharMagic Opens at Magic Kingdom",
+    description:
+      "This attraction, which is located in Fantasyland, starts Donald Duck and Mickey Mouse, and features a 4-D adventure through some of Disney Animation's greatest musical movie moments."
+  } , {
+    park: "mk",
+    date: "October 9, 2003",
+    title: "Wishes: A Magical Gathering of Disney Dreams Debuts at Magic Kingdom",
+    description:
+      "This nighttime spectacular, hosted by Jiminy Cricket and the Blue Fairy, features fireworks launched over both Cinderella Castle and Beast's Castle, changing colors on Cinderella Castle, and a special appearance by Tinker Bell. The show's last performance was May 11, 2017, as it was retired to make room for a new nighttime spectacular."
+  } , {
+    park: "resort",
+    date: "December 14, 2003",
+    title: "Disney's Pop Century Resort Opens",
+    description:
+      "This value resort, located near ESPN's Wide World of Sports, celebrates pop culture from 5 decades: the 1950's, 60's, 70's, 80's, and 90's. A second part of the resort, celebrating the first half of the 20th century, was planned but never built."
+  } , {
+    park: "resort",
+    date: "May 17, 2004",
+    title: "Disney's Saratoga Springs Resort & Spa Opens",
+    description:
+      "This Disney Vacation Club property, located near Disney Springs, was inspired by the city of Saratoga Springs, New York, and contains 18 buildings of villas, as well as 60 Treehouse Villas. It is one of two resorts built exclusively for Disney Vacation Club villas (however, the villas are bookable as regular hotel rooms)."
+  } , {
+    park: "ep",
+    date: "November 16, 2004",
+    title: "Turtle Talk with Crush Opens",
+    description:
+      "This interactive attraction, housed in the Living Seas pavillion, allows audience members to interact with the sea turtle Crush, a character from Pixar's <em>Finding Nemo</em>. Crush answers questions from the audience about life in the sea and other topics."
+  } , {
+    park: "mk",
+    date: "November 18, 2004",
+    title: "Stitch's Great Escape Opens at Magic Kingdom",
+    description:
+      "This attraction, which features the title alien from <em>Lilo & Stitch</em>, is located in the building that formerly housed ExtraTERRORestrial Alien Encounter. It currently operates as a seasonal attraction."
+  } , {
+    park: "resort",
+    date: "March 15, 2005",
+    title: "Crush ‘n’ Gusher Opens at Typhoon Lagoon",
+    description:
+      "This ride was the first water coaster to be built for a Disney water park, and features three different slides, the 'Banana Blaster', 'Coconut Crusher', or 'Pineapple Plunger,' which range from 410 to 420 feet in length, but all send riders to a plunge into the 'Hideaway Bay' pool."
+  } , {
+    park: "ep",
+    date: "May 5, 2005",
+    title: "Soarin' Opens at Epcot",
+    description:
+      "This attraction, located in The Land pavillion, originally took riders on an aerial tour of California. The attraction closed for a several month refurbishment in 2016, and shortly after it reopened, on June 17, 2016, the film was changed to an aerial tour of the world called <em>Soarin' Around the World</em>."
+  } , {
+    park: "dhs",
+    date: "May 15, 2005",
+    title: "Lights, Motors, Action! Extreme Stunt Show Debuts",
+    description:
+      "This stunt show, located in the Backlot area of Disney-MGM Studios (later Hollywood Studios), was brought over from the Walt Disney Studios Park in Paris as part of the Happiest Celebration on Earth event (celebrating 50 years of Disney Parks). The show closed in April of 2016 to make room for expansions at Hollywood Studios."
+  } , {
+    park: "dhs",
+    date: "December 9, 2005",
+    title: "Journey into Narnia: Creating the Lion, the Witch, and the Wardrobe Opens",
+    description:
+      "This stand and watch attraction operated at Disney-MGM Studios from December 2005 to January 1, 2008, when it closed to make room for Journey into Narnia: Prince Caspian, based on a newer installment in the same film franchise."
+  } , {
+    park: "ak",
+    date: "January 21, 2006",
+    title: "Tarzan Rocks! Closes at Animal Kindom",
+    description:
+      "This show, which was held in the Theater in the Wild in Dinoland U.S.A., featured rollerskating monkeys along with Tarzan, Jane and Terk. It also featured songs from the 1999 film <em>Tarzan</em>."
+  } , {
+    park: "ak",
+    date: "April 7, 2006",
+    title: "Expedition Everest Opens at Animal Kingdom",
+    description:
+      "This steel coaster, located in the Asia section of Animal Kingdom, is themed around the Yeti hiding in Mount Everest. It is the tallest artificial mountain in the Disney Parks, and was the first Disney roller coaster to switch between forward and backward sections during a single ride."
+  } , {
+    park: "ak",
+    date: "January 1, 2007",
+    title: "Finding Nemo – The Musical Debuts",
+    description:
+      "This show, which is located in the Theater in the Wild in Animal Kingdom's Dinoland U.S.A., is based on the popular Pixar film <em>Finding Nemo</em> and features new songs written by Robert Lopez and Kristen Anderson-Lopez, as well as puppets, dancers, acrobats and animated backdrops."
+  } , {
+    park: "ep",
+    date: "January 24, 2007",
+    title: "The Seas with Nemo & Friends Opens at Epcot",
+    description:
+      "This area, known from opening day until August 21, 2005 as 'The Living Seas,' houses an aquarium, a dark ride themed around <em>Finding Nemo</em>, and an interactive attraction themed around the sea turtle Crush."
+  } , {
+    park: "mk",
+    date: "April 2, 2007",
+    title: "Monsters, Inc. Laugh Floor Opens at Magic Kingdom",
+    description:
+      "This attraction, located in Tomorrowland, invites guests into the world of the Pixar film <em>Monsters, Inc.</em>, where they are entertained at Monstropolis' first comedy club. Guests are given the ability to submit jokes to be used in the show, and guests have an opportunity to interact with the show."
+  } , {
+    park: "ep",
+    date: "April 6, 2007",
+    title: "Gran Fiesta Tour Starring The Three Caballeros Opens",
+    description:
+      "This attraction, located in Epcot's Mexico pavillion, is a musical dark boat ride that replaced El Rio del Tiempo. It was the first World Showcase attraction to feature Disney characters based on an existing property, and features Panchito Pistoles, José Carioca, and Donald Duck, from the 1944 film <em>The Three Caballeros</em>."
+  } , {
+    park: "mk",
+    date: "September 10, 2007",
+    title: "Bibbidi Bobbidi Boutique Opens at Magic Kingdom",
+    description:
+      "This enchanted beauty salon, located inside Cinderella Castle, offers serveral magical makover packages where a 'Fairy Godmother in Training' will transform young children into princesses or knights. This is the second Bibbidi Bobbidi Boutique location at Walt Disney World, with the first location inside Downtown Disney's World of Disney store."
+  } , {
+    park: "dhs",
+    date: "January 7, 2008",
+    title: "Disney-MGM Studios Becomes Disney's Hollywood Studios",
+    description:
+      "This name change, announced in August 2007, marked the end of a partnership between Disney and Metro-Goldwyn-Mayer. The original name-change announcement also included the announcement of several new attractions, which opened in 2007 and 2008."
+  } , {
+    park: "ep",
+    date: "February 19, 2008",
+    title: "Spaceship Earth Reopens After Refurbishment",
+    description:
+      "The attraction, located in Epcot's famous geodesic sphere, reopens after a 7-month refurbishment with a new sponsor, Siemens, and a new narrator, Dame Judi Dench. The update also included new scenes, modifications to existing scenes, some new costumes, lighting, and props, a new musical score by Bruce Broughton."
+  } , {
+    park: "dhs",
+    date: "May 31, 2008",
+    title: "Toy Story Midway Mania! Opens at Hollywood Studios",
+    description:
+      "This attraction, located in the Pixar Place section of the park, allows guests wearing 3-D glasses to play five mini-games based on classic carnival midway games. The attraction queue features a large Mr. Potato Head Audio-Animatronics figure that interacts with guests through pre-recorded snippets of dialogue."
+  } , {
+    park: "dhs",
+    date: "June 27, 2008",
+    title: "Journey into Narnia: Prince Caspian Opens at Hollywood Studios",
+    description:
+      "This one-room walkthrough attraction featured trivia, behind-the-scenes footage, sets, clips, art, storyboards, props, and costumes from the film <em>The Chronicles of Narnia: Prince Caspian</em>. It closed on September 10, 2011 and was replaced by The Legend Of Captain Jack Sparrow."
+  } , {
+    park: "dhs",
+    date: "February 14, 2009",
+    title: "The American Idol Experience Opens",
+    description:
+      "This attraction, located at the Hyperion Theater in the Echo Lake section of Hollywood Studios, invited park guests to audition in front of 'casting directors' and then live audiences, giving the audiences opportunities to vote for their favorite singers. The final show of each day featured the winners of the previous shows, and the winner of this show was given a 'Dream Ticket' that provided the opportunity to bypass the queue at a regional <em>American Idol</em> taping. The attraction closed on August 30, 2014."
+  } , {
+    park: "rD",
+    date: "March 3, 2009",
+    title: "Inaugural Princess Half-Marathon Weekend Begins",
+    description:
+      "This female-themed race weekend, celebrating princesses of all kinds, featured two races in its inagural year, the Royal Family 5K and Princess Half-Marathon, as well as a Health and Fitness Expo."
+  } , {
+    park: "resort",
+    date: "May 1, 2009",
+    title: "Kidani Village opens at Disney's Animal Kingdom Lodge",
+    description:
+      "Kidani Village, a Disney Vacation Club property, is the second building at Animal Kingdom, and features views of animals in a savanna environment from both public areas and selected rooms."
+  } , {
+    park: "resort",
+    date: "August 4, 2009",
+    title: "Bay Lake Tower at Disney's Contemporary Resort Opens",
+    description:
+      "This resort, a Disney Vacation Club properly located adjacent to Magic Kingdom, occupies the land formerly occupied by a garden wing of the original A-frame Contemporary Tower. It features its own pool, water feature play area, pool bar, bocce court, Community Hall featuring games and family activities, guest laundry room, but shares some amenities with the Contemporary Resort."
+  } , {
+    park: "ep",
+    date: "October 14, 2009",
+    title: "The Sum of All Thrills Opens at Epcot",
+    description:
+      "This attraction, located within the Innoventions pavillion, was a virtual roller coaster that allowed park guests custom-design their own thrill ride using mathematical tools, an innovative touch-screen table and a robotic simulator. It closed permanently on September 14, 2016."
+  }
+];
